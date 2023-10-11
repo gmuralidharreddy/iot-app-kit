79398dcb24d66ace7c0e459aabeb1f4acc712908
Murali <gmuralidhar.reddy@happiestminds.com>
2023-10-12 00:40:06 +0530

    feat: fixed decimal places in resource explorer

--- packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/createModeledDataStreamColumnDefinitions.ts
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/createModeledDataStreamColumnDefinitions.ts
@@ -3,7 +3,8 @@ import { type TableProps } from '@cloudscape-design/components/table';
 import { useLatestValues } from '../../../useLatestValues';
 
 export function createModeledDataStreamColumnDefinitions(
-  getLatestValue: ReturnType<typeof useLatestValues>['getLatestValue']
+  getLatestValue: ReturnType<typeof useLatestValues>['getLatestValue'],
+  significantDigits: number
 ): TableProps<
   ModeledDataStream & { latestValue?: number | string | boolean; latestValueTime?: number }
 >['columnDefinitions'] {
@@ -17,14 +18,25 @@ export function createModeledDataStreamColumnDefinitions(
     {
       id: 'latestValue',
       header: 'Latest value',
-      cell: ({ assetId = '', propertyId = '' }) => getLatestValue({ assetId, propertyId } as ModeledDataStream)?.value,
+      cell: ({ assetId = '', propertyId = '' }) => {
+        const latestValue = getLatestValue({ assetId, propertyId } as ModeledDataStream)?.value ?? '';
+        if (typeof latestValue === 'number' && !Number.isInteger(latestValue)) {
+          return latestValue.toFixed(significantDigits);
+        }
+        return latestValue;
+      },
       sortingField: 'latestValue',
     },
     {
       id: 'latestValueTime',
       header: 'Latest value time',
-      cell: ({ assetId = '', propertyId = '' }) =>
-        getLatestValue({ assetId, propertyId } as ModeledDataStream)?.timestamp,
+      cell: ({ assetId = '', propertyId = '' }) => {
+        const latestValueTime = getLatestValue({ assetId, propertyId } as ModeledDataStream)?.timestamp ?? '';
+        if (typeof latestValueTime === 'number') {
+          return latestValueTime.toFixed(significantDigits);
+        }
+        return latestValueTime;
+      },
       sortingField: 'latestValueTime',
     },
     {
